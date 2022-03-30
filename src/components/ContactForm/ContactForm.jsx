47535cb725dedf21e4eb466cda1b24c64c7eb907
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2022-03-30 12:15:41 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -39,6 +39,8 @@ class ContactForm extends Component {
           value={this.state.name}
           type="text"
           name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={this.handleChange}
         />
